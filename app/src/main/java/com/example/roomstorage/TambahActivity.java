b46d4362ee0702ee9b77b872c582566b8893a74c
devidwiyani <devidwiyani52@gmail.com>
2021-10-25 23:30:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/roomstorage/TambahActivity.java
@@ -0,0 +1,55 @@
+package com.example.roomstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.roomstorage.database.AppDatabase;
+import com.example.roomstorage.database.entitas.User;
+
+public class TambahActivity extends AppCompatActivity {
+    private EditText editName, editEmail;
+    private Button btnSave;
+    private AppDatabase database;
+    private int uid = 0;
+    private boolean isEdit = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah);
+        editName = findViewById(R.id.full_name);
+        editEmail = findViewById(R.id.email);
+        btnSave = findViewById(R.id.btn_save);
+
+        database = AppDatabase.getInstance(getApplicationContext());
+
+        Intent intent = getIntent();
+        uid = intent.getIntExtra("uid", 0);
+        if (uid>0){
+            isEdit = true;
+            User user = database.userDao().get(uid);
+            editEmail.setText(user.email);
+            editName.setText(user.fullName);
+        }else{
+            isEdit = false;
+        }
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if  (isEdit){
+                    database.userDao().update(uid, editName.getText().toString(), editEmail.getText().toString());
+                }else{
+                    database.userDao().insertAll(editName.getText().toString(), editEmail.getText().toString());
+                }
+                finish();
+            }
+        });
+    }
+}
+
